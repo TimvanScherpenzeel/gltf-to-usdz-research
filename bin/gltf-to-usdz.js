66ccdfed618bc085968a5e20b88679bfa995d805
Tim van Scherpenzeel <timvanscherpenzeel@gmail.com>
2018-08-26 21:57:00 +0200

    added prettier and fixed filename issues

--- bin/gltf-to-usdz.js
+++ bin/gltf-to-usdz.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+
 // Native
 const fs = require('fs');
 const path = require('path');
@@ -22,40 +23,40 @@ const inputFileExtension = path.extname(input);
 const outputFileExtension = path.extname(output);
 
 if (!SUPPORTED_OUTPUT_TYPES.includes(outputFileExtension)) {
-	console.error(`${outputFileExtension} is not supported.`);
-	console.error(`The supported filetypes are: [${SUPPORTED_OUTPUT_TYPES}]`);
-	process.exit(1);
+  console.error(`${outputFileExtension} is not supported.`);
+  console.error(`The supported filetypes are: [${SUPPORTED_OUTPUT_TYPES}]`);
+  process.exit(1);
 }
 
 if (!SUPPORTED_INPUT_TYPES.includes(inputFileExtension)) {
-	console.error(`${inputFileExtension} is not supported.`);
-	console.error(`The supported file extensions are: [${SUPPORTED_INPUT_TYPES}]`);
+  console.error(`${inputFileExtension} is not supported.`);
+  console.error(`The supported file extensions are: [${SUPPORTED_INPUT_TYPES}]`);
 }
 
 fileLoader(input, 'utf8')
-	.then((gltf) => {
-		const parsedGltf = JSON.parse(gltf);
+  .then((gltf) => {
+    const parsedGltf = JSON.parse(gltf);
 
-		gltfValidator
-			.validateBytes(new Uint8Array(Buffer.from(JSON.stringify(parsedGltf))))
-			.catch((error) => {
-				console.warn('Validation failed: ', error);
-				process.exit(1);
-			});
+    gltfValidator
+      .validateBytes(new Uint8Array(Buffer.from(JSON.stringify(parsedGltf))))
+      .catch((error) => {
+        console.warn('Validation failed: ', error);
+        process.exit(1);
+      });
 
-		return converter(parsedGltf);
-	})
-	.then((data) => {
-		try {
-			fs.writeFileSync(output, data);
-		} catch (err) {
-			return Promise.reject(err);
-		}
+    return converter(parsedGltf);
+  })
+  .then((data) => {
+    try {
+      fs.writeFileSync(output, data);
+    } catch (err) {
+      return Promise.reject(err);
+    }
 
-		console.log(`Written file to ${output}`);
-		return Promise.resolve();
-	})
-	.catch((error) => {
-		console.error(error);
-		process.exit(1);
-	});
+    console.log(`Written file to ${output}`);
+    return Promise.resolve();
+  })
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
